e2b8dd4c8e760d0e3ff1dc8cf3a3ea93c027b79b
Jules Bayon de Noyer <julesbn@hotmail.fr>
2019-11-12 16:41:43 +0100

    finished last exercise but still no front

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times {
+  article = Article.new(title: "#{Faker::GreekPhilosophers.name} once said", content: Faker::TvShows::HowIMetYourMother.quote)
+  article.save
+}
